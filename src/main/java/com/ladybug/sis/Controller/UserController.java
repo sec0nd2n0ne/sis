9ac6c5ba1f59e0cc0ad90d9f561073c5da6bce13
Rowel Casulla <rcasulla@nmcgroup.com.au>
2019-08-07 09:45:59 +0800

    Working REST Crud

--- src/main/java/com/ladybug/sis/Controller/UserController.java
+++ src/main/java/com/ladybug/sis/Controller/UserController.java
@@ -3,31 +3,82 @@ package com.ladybug.sis.Controller;
 import com.ladybug.sis.Model.User;
 import com.ladybug.sis.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping(value = "/")
-    public List<User> findAll(){
+    @GetMapping()
+    public @ResponseBody List<User> findAll(){
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public User findById(@PathVariable(value = "id") Long userID){
-        return userService.findById(userID);
+    public ResponseEntity<User> findById(@PathVariable(value = "id") Long userID){
+        User foundUser = userService.findById(userID);
+
+        if (foundUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(foundUser);
+
     }
 
-    @PostMapping("/new")
-    public User addUser(@Valid @RequestBody User user){
-        return userService.addUser(user);
+    @PostMapping("/")
+    public ResponseEntity<User> addUser(@RequestBody User user) throws URISyntaxException {
+        User addedUser = userService.addOrUpdateUser(user);
+
+        if (addedUser == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(addedUser.getUserID())
+                    .toUri();
+
+        return ResponseEntity.created(uri)
+                .body(addedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable(value = "id") long userID){
+        User userToUpdate = userService.findById(userID);
+
+        if (userToUpdate == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setUserID(userID);
+        User updatedUser = userService.addOrUpdateUser(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(updatedUser.getUserID())
+                .toUri();
+
+        return ResponseEntity.created(uri)
+                .body(updatedUser);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") long userID){
+        userService.deleteUser(userID);
+
+        return ResponseEntity.noContent().build();
+
     }
 
 }
--- src/main/java/com/ladybug/sis/Model/User.java
+++ src/main/java/com/ladybug/sis/Model/User.java
@@ -1,7 +1,12 @@
 package com.ladybug.sis.Model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Setter
+@Getter
+@Data
 @Entity
 @Table(name = "tblUser")
 public class User {
@@ -31,126 +36,4 @@ public class User {
     @Column(name = "companyID")
     public long companyID;
 
-    public User() {
-    }
-
-    public User(int userID, int userProfileID, String userName, String password, String email, String firstName, String lastName, String phone, String mobile, String gender, int companyID) {
-        this.userID = userID;
-        this.userProfileID = userProfileID;
-        this.userName = userName;
-        this.password = password;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.mobile = mobile;
-        this.gender = gender;
-        this.companyID = companyID;
-    }
-
-
-    public long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
-    public long getUserProfileID() {
-        return userProfileID;
-    }
-
-    public void setUserProfileID(int userProfileID) {
-        this.userProfileID = userProfileID;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getCompanyID() {
-        return companyID;
-    }
-
-    public void setCompanyID(int companyID) {
-        this.companyID = companyID;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userID=" + userID +
-                ", userProfileID=" + userProfileID +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phone='" + phone + '\'' +
-                ", mobile='" + mobile + '\'' +
-                ", gender='" + gender + '\'' +
-                ", companyID=" + companyID +
-                '}';
-    }
 }
